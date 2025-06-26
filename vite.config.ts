a4e09ed985fa32b3f61b7847fceb961ae23d1383
vitalspace <myglobalsounds@gmail.com>
2025-06-26 13:04:40 -0600

    Fix global is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
+  define: {
+    global: 'globalThis',
+  },
 })
