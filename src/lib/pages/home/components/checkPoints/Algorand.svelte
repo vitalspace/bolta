ab76de2d505dd8a2bb2184dd9fe1515b48f76456
vitalspace <myglobalsounds@gmail.com>
2025-06-26 11:18:04 -0600

    Updated Algorand.svelte

---
+++ src/lib/pages/home/components/checkPoints/Algorand.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { T } from "@threlte/core"
+    
+</script>
+
+
+    <T.Group>
+      <AutoColliders
+        shape="cuboid"
+        sensor={true}
+        onsensorenter={() => {}}
+        onsensorexit={() => {}}
+      >
+        <T.Mesh position={[55.69, 3.3, -40.19]}>
+          <T.CylinderGeometry args={[0.4, 0.4, 0.5]} />
+          <FakeGlowMaterial
+            glowColor="green"
+            falloff={1}
+            glowInternalRadius={1}
+            glowSharpness={5}
+            depthTest={true}
+          />
+        </T.Mesh>
+      </AutoColliders>
